67c4b05de9823cc510fe51fc5f4a6c5721d53a76
ixchelmn <64459748+ixchelmn@users.noreply.github.com>
2023-02-28 23:42:18 -0600

    Cambios finales 23:41 28/02/2023

--- app/src/main/java/com/example/tarea2pm1p1/ActivityResultados.java
+++ app/src/main/java/com/example/tarea2pm1p1/ActivityResultados.java
@@ -21,10 +21,19 @@ public class ActivityResultados extends AppCompatActivity {
         //Get the bundle
         Bundle extras = getIntent().getExtras();
         String nombre = extras.getString("Nombre");
+        String apellido = extras.getString("Apellido");
+        String edad = extras.getString("Edad");
+        String correo = extras.getString("Correo");
 
         tv_resultado_nombre = findViewById(R.id.tv_resultado_nombre);
         tv_resultado_apellido =findViewById(R.id.tv_resultado_apellido);
         tv_resultado_edad = findViewById(R.id.tv_resultado_edad);
+        tv_resultado_correo = findViewById(R.id.tv_resultado_correo);
+
+        tv_resultado_nombre.setText("Nombre: " + nombre);
+        tv_resultado_apellido.setText("Apellido: " + apellido);
+        tv_resultado_edad.setText("Edad: " + edad);
+        tv_resultado_correo.setText("Correo: " + correo);
 
 
 
